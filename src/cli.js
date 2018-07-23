e6fd9d00cade4f366c2b1988fb1223c12bc0de54
scorsi <sylvain.corsini@gmail.com>
2018-07-23 22:07:34 +0200

    Check if the file exists and is a file.

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,7 @@ const chalk = require('chalk');
 const compile = require('./compile');
 const log = console.log;
 const TypesInstaller = require('./types-installer').TypesInstaller;
+const fs = require('fs');
 
 let antlrGrammars;
 const installer = new TypesInstaller();
@@ -33,6 +34,16 @@ config.visitor = opts['visitor'];
 config.listener = opts['listener'];
 
 log(`Compiling ${antlrGrammars.join(', ')}...`);
+
+_.each(antlrGrammars, (file) => {
+    if (fs.existsSync(file) === false) {
+        log(`The file ${file} doesn't exists.`);
+        process.exit(1);
+    } else if (fs.statSync(file).isFile() === false) {
+        log(`${file} is not a file.`);
+        process.exit(1);
+    }
+});
 
 const compileResults = compile(config);
 _.each(compileResults, (files, grammar) => {
